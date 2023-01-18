8af9c2cfee65cc69718f3cc02eff267f2a6e9793
Luciano Scabora <lucianoscabora@gmail.com>
2023-01-18 04:36:21 -0300

    completed habits route finished

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -31,4 +31,51 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
     });
-}
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        // load every habit that day
+        // load every completed habit
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findFirst({
+            where: {
+              date: parsedDate.toDate(),
+            },
+            include: {
+              dayHabits: true,
+            }
+          })
+      
+          const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+          })
+      
+          return {
+            possibleHabits,
+            completedHabits,
+          }
+        })
+      }
